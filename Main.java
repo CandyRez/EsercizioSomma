d7dda6b336166ed9942cdda28010adea6127f627
CandyRez <121175251+CandyRez@users.noreply.github.com>
2022-12-23 21:00:44 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("insert variables");
+        float x = input.nextInt();
+        float y = input.nextInt();
+
+        System.out.println(x + y);
+    }
+}
